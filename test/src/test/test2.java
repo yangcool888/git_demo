2dbe9145d07c31fa6db2bc2fbdbf7fcd7b3b14f3
yangshuang <yangshuang@Ctrip.com>
2016-05-09 16:13:54 +0800

    test005

--- test/src/test/test2.java
+++ test/src/test/test2.java
@@ -2,4 +2,13 @@ package test;
 
 public class test2 {
 
+	   System.out.println("ddd11111"); 
+	   System.out.println("≤‚ ‘003");
+	   System.out.println("≤‚ ‘004");
+	   System.out.println(" ≤‚ ‘ 001");
+	   System.out.println(" ≤‚ ‘ 002");
+	   System.out.println(" ≤‚ ‘ 001");
+	   System.out.println(" ≤‚ ‘ 002");
+	   System.out.println(" ≤‚ ‘ 001");
+	   System.out.println(" ≤‚ ‘ 002");
 }
