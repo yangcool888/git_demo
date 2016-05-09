c90cafcc1213e78e88af3a0bf3dcad26f44546e5
yangshuang <yangshuang@Ctrip.com>
2016-05-09 16:18:24 +0800

    test005

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,34 +1,37 @@
 package test;
 
 public class test {
-   System.out.println("ddd9999");
-   System.out.println("ddd66666");
-   System.out.println("ddd");
-   System.out.println("ddd4444");
-   System.out.println("ddd555555");
-   System.out.println("ddd77777");
-   System.out.println("ddd88888");
-   System.out.println("ddd22222222");
-   System.out.println("ddd99999");
+	public static void main(String[] args){
+		System.out.println("ddd9999");
+		   System.out.println("ddd66666");
+		   System.out.println("ddd");
+		   System.out.println("ddd4444");
+		   System.out.println("ddd555555");
+		   System.out.println("ddd77777");
+		   System.out.println("ddd88888");
+		   System.out.println("ddd22222222");
+		   System.out.println("ddd99999");
 
-   System.out.println("ddd11111"); 
-   System.out.println("≤‚ ‘003");
-   System.out.println("≤‚ ‘004");
-   System.out.println(" ≤‚ ‘ 001");
-   System.out.println(" ≤‚ ‘ 002");
-   System.out.println(" ≤‚ ‘ 001");
-   System.out.println(" ≤‚ ‘ 002");
-   System.out.println(" ≤‚ ‘ 001");
-   System.out.println(" ≤‚ ‘ 002");
+		   System.out.println("ddd11111"); 
+		   System.out.println("≤‚ ‘003");
+		   System.out.println("≤‚ ‘004");
+		   System.out.println(" ≤‚ ‘ 001");
+		   System.out.println(" ≤‚ ‘ 002");
+		   System.out.println(" ≤‚ ‘ 001");
+		   System.out.println(" ≤‚ ‘ 002");
+		   System.out.println(" ≤‚ ‘ 001");
+		   System.out.println(" ≤‚ ‘ 002");
+		   
+		   System.out.println("ddd2222222"); 
+		   System.out.println("≤‚ ‘003");
+		   System.out.println("≤‚ ‘004");
+		   System.out.println(" ≤‚ ‘ 001");
+		   System.out.println(" ≤‚ ‘ 002");
+		   System.out.println(" ≤‚ ‘ 001");
+		   System.out.println(" ≤‚ ‘ 002");
+		   System.out.println(" ≤‚ ‘ 001");
+		   System.out.println(" ≤‚ ‘ 002");
+	}
    
-   System.out.println("ddd2222222"); 
-   System.out.println("≤‚ ‘003");
-   System.out.println("≤‚ ‘004");
-   System.out.println(" ≤‚ ‘ 001");
-   System.out.println(" ≤‚ ‘ 002");
-   System.out.println(" ≤‚ ‘ 001");
-   System.out.println(" ≤‚ ‘ 002");
-   System.out.println(" ≤‚ ‘ 001");
-   System.out.println(" ≤‚ ‘ 002");
 
 } 
--- test/src/test/test2.java
+++ test/src/test/test2.java
@@ -1,14 +1,16 @@
 package test;
 
 public class test2 {
+	public static void main(String[] args) {
+		System.out.println("ddd11111");
+		System.out.println("≤‚ ‘003");
+		System.out.println("≤‚ ‘004");
+		System.out.println(" ≤‚ ‘ 001");
+		System.out.println(" ≤‚ ‘ 002");
+		System.out.println(" ≤‚ ‘ 001");
+		System.out.println(" ≤‚ ‘ 002");
+		System.out.println(" ≤‚ ‘ 001");
+		System.out.println(" ≤‚ ‘ 002");
+	}
 
-	   System.out.println("ddd11111"); 
-	   System.out.println("≤‚ ‘003");
-	   System.out.println("≤‚ ‘004");
-	   System.out.println(" ≤‚ ‘ 001");
-	   System.out.println(" ≤‚ ‘ 002");
-	   System.out.println(" ≤‚ ‘ 001");
-	   System.out.println(" ≤‚ ‘ 002");
-	   System.out.println(" ≤‚ ‘ 001");
-	   System.out.println(" ≤‚ ‘ 002");
 }
