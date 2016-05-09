47c53f9e440a15ec624bab002a03aa5908be1c7e
yangshuang <yangshuang@Ctrip.com>
2016-05-09 14:44:37 +0800

    test 44

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,8 +1,6 @@
 package test;
 
 public class test {
-	System.out.println("ddd1111");
    System.out.println("ddd");
-   System.out.println("ddd1111");
-   System.out.println("ddd2222");
+   System.out.println("ddd4444");
 } 
